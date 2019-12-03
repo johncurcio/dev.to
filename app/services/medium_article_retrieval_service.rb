094c8747fe4ef87ee557365f44b934277ef745e9
John Curcio <john@curcio.dev>
2019-12-02 21:57:10 -0300

    Add publication date to medium liquid tag

--- app/services/medium_article_retrieval_service.rb
+++ app/services/medium_article_retrieval_service.rb
@@ -17,12 +17,15 @@ class MediumArticleRetrievalService
     reading_time = page.at("meta[name='twitter:data1']")["value"]
     author = page.at("meta[name='author']")["content"]
     author_image = page.at("img[alt='#{author}']")["src"]
+    published_time = page.at("meta[property='article:published_time']")["content"]
+    publication_date = DateTime.parse(published_time).strftime("%b %-d, %Y")
 
     {
       title: title,
       author: author,
       author_image: author_image,
       reading_time: reading_time,
+      publication_date: publication_date,
       url: url
     }
   end
